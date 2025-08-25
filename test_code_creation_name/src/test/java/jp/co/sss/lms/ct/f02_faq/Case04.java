40de3baf960520d994808de94113083e04f6d6e0
g-hinata <g27itschool@gmail.com>
2025-08-25 16:06:03 +0900

    ADD:Case04.java よくある質問画面への遷移テストケースを追加
    ADD:CT_F01_04_login_screen.png Case4テストのエビデンス
    FIX:結合テスト仕様書 ケース３、ケース４の仕様書追加
    
    ・コース詳細画面からヘルプ画面に遷移後、ヘルプ画面からよくある質問画面へ正常に遷移することを確認するテストを追加。
    	-コース詳細画面から上部のドロップダウンリストを介してヘルプ画面に遷移する検証
    	-ヘルプ画面の要素表示確認
    	-ヘルプ画面から別タブで開く「よくある質問」画面への遷移検証
    	-よくある質問画面の要素表示確認
    	-画面のスクリーンショット取得

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
@@ -1,7 +1,17 @@
 package jp.co.sss.lms.ct.f02_faq;
 
-import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static jp.co.sss.lms.ct.util.WebDriverUtils.closeDriver;
+import static jp.co.sss.lms.ct.util.WebDriverUtils.createDriver;
+import static jp.co.sss.lms.ct.util.WebDriverUtils.visibilityTimeout;
+import static jp.co.sss.lms.ct.util.WebDriverUtils.webDriver;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -9,6 +19,9 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 /**
  * 結合テスト よくある質問機能
@@ -35,28 +48,71 @@ public class Case04 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		//トップページへアクセス
+		webDriver.get("http://localhost:8080/lms");
+		//URLでログイン画面へ遷移か検証
+		assertEquals("http://localhost:8080/lms/", webDriver.getCurrentUrl());
+
+		//ログインID、パスワード、ログインボタン検証
+		assertTrue(webDriver.findElement(By.id("loginId")).isDisplayed());
+		assertTrue(webDriver.findElement(By.id("password")).isDisplayed());
+		assertTrue(webDriver.findElement(By.xpath("//input[@value='ログイン']")).isDisplayed());
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		//トップページへアクセス
+		webDriver.get("http://localhost:8080/lms");
+		//URLでログイン画面へ遷移か検証
+		assertEquals("http://localhost:8080/lms/", webDriver.getCurrentUrl());
+		//DBに登録されているユーザ情報を入力
+		webDriver.findElement(By.id("loginId")).sendKeys("StudentAA01");
+		webDriver.findElement(By.id("password")).sendKeys("StudenTAA01");
+		//ログインボタンをクリック
+		webDriver.findElement(By.xpath("//input[@value='ログイン']")).click();
+		//コース詳細画面の要素が表示されるまで待機
+		visibilityTimeout(By.className("clearfix"), 10);
+
+		//URLへ正常に遷移することの検証
+		assertEquals("http://localhost:8080/lms/course/detail", webDriver.getCurrentUrl());
+
+		//コース詳細画面の要素を確認
+		assertTrue(webDriver.findElement(By.id("contents")).isDisplayed());
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		//ドロップダウンリストをクリック
+		webDriver.findElement(By.xpath("//*[@id=\"nav-content\"]/ul[1]/li[4]/a")).click();
+		//「ヘルプ」リンクをクリック
+		webDriver.findElement(By.xpath("//*[@id=\"nav-content\"]/ul[1]/li[4]/ul/li[4]/a")).click();
+		//ヘルプ画面のURLを検証
+		assertEquals("http://localhost:8080/lms/help", webDriver.getCurrentUrl());
+		//よくある質問の要素の表示の検証
+		assertTrue(webDriver.findElement(By.xpath("//*[@id=\"main\"]/div[2]/div[2]/p/a")).isDisplayed());
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
-	void test04() {
-		// TODO ここに追加
+	void test04() throws IOException {
+		// 「よくある質問」リンクをクリック
+		webDriver.findElement(By.xpath("//*[@id=\"main\"]/div[2]/div[2]/p/a")).click();
+		// ウィンドウの操作を取得
+		ArrayList<String> tabs = new ArrayList<>(webDriver.getWindowHandles());
+		// 2番目のタブに切り替え
+		webDriver.switchTo().window(tabs.get(1));
+		// よくある質問画面のURLを検証
+		assertEquals("http://localhost:8080/lms/faq", webDriver.getCurrentUrl());
+		// 期待する要素表示の検証
+		assertTrue(webDriver.findElement(By.xpath("//*[@id=\"main\"]/h2")).isDisplayed());
+		//スクリーンショットを取得して保存
+		File file = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(file, new File(
+				"C:\\\\\\\\Users\\\\\\\\user\\\\\\\\git\\\\\\\\lms-test-src-rp2\\\\\\\\test_code_creation_name\\\\\\\\evidence\\\\\\\\CT_F01_04_login_screen.png"));
 	}
-
 }
